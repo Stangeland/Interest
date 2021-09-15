c59ec4e80d17ba29ac541a1828ceae00da704441
Stangeland <Stangeland@outlook.com>
2021-09-15 20:35:00 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'interest';
+  total;
+  interest = 3.5;
+  amount = 100000;
+  time = 30;
+   Payment =  {
+    remainder:  new Array,
+    interest:  new Array,
+    monthly:  new Array,
+    progress:  new Array,
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  interestSlider(event: MatSliderChange) {
+    this.interest = event.value ? event.value : 0 ;
+  }
+  
+
+  yearSlider(event: MatSliderChange) {
+    this.time = event.value ? event.value : 0 ;
+  }
+
+
+  paymentPlan() {
+    this.Payment.interest = [];
+    this.Payment.monthly = [];
+    this.Payment.remainder = [];
+    this.Payment.progress = [];
+    const _interest = this.interest/100;
+    const periodicRate = _interest /12;
+    const n = this.time *12;
+    let remainder = this.amount;
+    let i = 0;
+    const x = Math.pow(1 + periodicRate, n);
+    const monthly = (remainder*x*periodicRate)/(x-1);
+    this.total = 0;
+    while (i <= n) {
+      this.Payment.remainder.push(remainder);
+      this.Payment.interest.push((remainder)*periodicRate);
+      this.Payment.monthly.push(monthly);
+
+      this.Payment.progress.push(this.Payment.monthly[i]- this.Payment.interest[i]);
+      
+      remainder = Math.round((remainder - this.Payment.monthly[i] +this.Payment.interest[i])*100)/100 ;
+      if (remainder < 0) { // prevents rounding errors
+        remainder = 0;
+      }
+      i++;
+    }
+    this.total = monthly * n;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {FormsModule} from '@angular/forms'
+import {MatFormFieldModule } from '@angular/material/form-field'
+import {MatInputModule } from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button'
 
 @NgModule({
   declarations: [
@@ -10,7 +16,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
